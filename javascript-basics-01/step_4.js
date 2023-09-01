6daff8047977f15860e378c5831c3571fa2f5313
whathow <marwa@gmail.com>
2023-09-01 09:55:14 +0300

    Finish step4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+const userName = prompt("Please enter your name:");
+const surname = prompt("Please enter your surname:");
+const city = prompt("Please enter your city:");
+alert("name : " + userName+ "\nsurname:" +surname+ "\ncity:" +city );
