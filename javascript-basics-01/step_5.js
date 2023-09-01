1982e395847eda9b40dfbf1100b79b6f9581f782
whathow <marwa@gmail.com>
2023-09-01 10:00:49 +0300

    Finish step5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,4 @@
+const num1 = prompt("Enter the first number:");
+ const num2 = prompt("Enter the second number:");
+ const result = num1 * num2;
+ alert("The result of multiplying " + num1+ " and " + num2 + " is: " + result);
