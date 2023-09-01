00bc9fe2b814de9c730833e9a1a627bf29e31570
whathow <marwa@gmail.com>
2023-09-01 10:07:04 +0300

    Finish step6

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,4 @@
+const num1= prompt("Enter the dividend (number to be divided):");
+const num2 = prompt("Enter the divisor (number to divide by):");
+const result= num1 / num2;
+alert("the result of division is " +result);
